9073d932f30c590f3b12b3f69ef721b43148997b
Tamal2005 <tamaldebnath2005@gmail.com>
2025-11-01 01:44:14 +0530

    initial commit

--- app/routes.ts
+++ app/routes.ts
@@ -4,7 +4,7 @@ export default [index("routes/home.tsx"),
     route("about","components/About.tsx"),
     route("overview","components/Overview.tsx"),    
     route("spam-mail","components/modelsPage/SpamMailPage.tsx"),
-    route("phishing-url","components/modelsPage/PhishingURLPage.tsx"),
+    route("phishing-url","components/modelsPage/PhishingUrlPage.tsx"),
     route("face-mask","components/modelsPage/FaceMaskPage.tsx"),
     route("deepfake-voice","components/modelsPage/DeepfakeVoicePage.tsx"),
 ] satisfies RouteConfig;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import Navbar from "~/components/Navbar";
 import HomePage from "~/components/Home";
 
 
